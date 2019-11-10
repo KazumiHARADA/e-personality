f00d3f748316fd6af003f863f9d2935c6dd7e1b0
Kazumi Harada <kazumi.h.apple48@gmail.com>
2019-11-10 21:34:07 +0900

    routing test

--- pages/index.vue
+++ pages/index.vue
@@ -20,6 +20,21 @@
           GitHub
         </a>
       </div>
+      <div>
+        <b-form-radio-group
+          v-model="value"
+          :options="options"
+          :state="state"
+          name="radio-validation"
+        >
+          <b-form-invalid-feedback :state="state"
+            >Please select one</b-form-invalid-feedback
+          >
+          <b-form-valid-feedback :state="state"
+            >Thank you</b-form-valid-feedback
+          >
+        </b-form-radio-group>
+      </div>
     </div>
   </div>
 </template>
@@ -30,6 +45,21 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  data() {
+    return {
+      value: null,
+      options: [
+        { text: 'First radio', value: 'first' },
+        { text: 'Second radio', value: 'second' },
+        { text: 'Third radio', value: 'third' }
+      ]
+    }
+  },
+  computed: {
+    state() {
+      return Boolean(this.value)
+    }
   }
 }
 </script>
