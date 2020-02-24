2d83478870b230c817837b56d99f1ddb3b701de6
Kazumi Harada <kazumi.h.apple48@gmail.com>
2020-02-24 22:12:08 +0900

    [update] seo設定

--- functions/nuxt.config.js
+++ functions/nuxt.config.js
@@ -1,29 +1,103 @@
 // import Questions from './assets/ja-edited-questions.json'
 
+const baseUrl =
+  'https://e-personality.firebaseapp.com' || 'http://localhost:3000'
+const baseDir = process.env.BASE_DIR || '/'
+const basePath = baseUrl + baseDir
+
+// meta
+const siteName = 'BigFive 精密パーソナリティ診断 ~ ePersonality ~'
+const shortName = 'ePersonality'
+const siteDesc = process.env.npm_package_description
+const siteKeywords = '性格診断,BigFive,パーソナリティ,性格分析'
+const manifestIcon = '/icon-1024.png'
+
 module.exports = {
   mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s | Big Five 精密 パーソナリティ診断 ~ e-personality ~',
+    titleTemplate: `%s | ${siteName}`,
     htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#',
       lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      },
+      // SEO関連
+      { hid: 'description', name: 'description', content: siteDesc },
+      { hid: 'keywords', name: 'keywords', content: siteKeywords },
       {
         name: 'google-site-verification',
         content: 'qXnJkdNM7nOPIkqEvkQCgErSloWOUFNN_8-ntMxc8Ws'
+      },
+
+      // ogp関連
+      { hid: 'og:site_name', property: 'og:site_name', content: siteName },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: basePath },
+      { hid: 'og:title', property: 'og:title', content: siteName },
+      { hid: 'og:description', property: 'og:description', content: siteDesc },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: `/ogpimage.png`
+      },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      {
+        name: 'apple-mobile-web-app-status-bar-style',
+        content: 'black-translucent'
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      // icons
+      {
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/png',
+        href: '/favicon-16.png'
+      },
+      {
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/png',
+        href: '/favicon-32.png'
+      },
+      {
+        rel: 'icon',
+        sizes: '48x48',
+        type: 'image/png',
+        href: '/favicon-48.png'
+      },
+      {
+        rel: 'icon',
+        sizes: '62x62',
+        type: 'image/png',
+        href: '/favicon-62.png'
+      },
+      // apple touch icon
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      }
+    ]
+  },
+  icon: {
+    iconFileName: manifestIcon
+  },
+  manifest: {
+    lang: 'ja',
+    name: siteName,
+    short_name: shortName,
+    description: siteDesc,
+    background_color: '#ffffff',
+    theme_color: '#ffffff',
+    display: 'standalone',
+    orientation: 'portrait'
   },
   /*
    ** Customize the progress-bar color
@@ -99,6 +173,6 @@ module.exports = {
   sitemap: {
     path: '/sitemap.xml',
     hostname: 'https://e-personality.firebaseapp.com/',
-    exclude: ['/result', '/inputs/1', '/simple/**']
+    exclude: ['/result', '/inputs/**', '/simple/**']
   }
 }
